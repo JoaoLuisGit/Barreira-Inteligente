f20623f20b286b1953a35dadcc0f11c9bd2a2bda
JoaoLuisGit <luis.joaoraujo126@gmail.com>
2023-07-30 00:38:21 -0300

    atualização p6

---
+++ V2.0/a.js
@@ -0,0 +1,28 @@
+function openTab(tabName) {
+    // Esconda todos os conteúdos das abas
+    var tabContents = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabContents.length; i++) {
+      tabContents[i].classList.remove('active');
+    }
+  
+    // Mostrar o conteúdo da aba selecionada
+    var selectedTab = document.getElementById(tabName);
+    selectedTab.classList.add('active');
+  }
+  js
+  function openTab(tabName) {
+    // Esconda todos os conteúdos das abas
+    var tabContents = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabContents.length; i++) {
+      tabContents[i].classList.remove('active');
+    }
+  
+    // Mostrar o conteúdo da aba selecionada
+    var selectedTab = document.getElementById(tabName);
+    selectedTab.classList.add('active');
+  
+    // Ocultar o conteúdo após 5 segundos
+    setTimeout(function() {
+      selectedTab.classList.remove('active');
+    }, 5000); // 5000 milissegundos = 5 segundos
+  }
